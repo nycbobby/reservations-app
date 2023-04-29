6313d6e1d471cafe7f5cf1ac659919c502291418
Bobby B <bobby.benninger@gmail.com>
2023-04-29 13:13:49 -0700

    update lambda  and policy

--- terraform/api_gw.tf
+++ terraform/api_gw.tf
@@ -101,7 +101,7 @@ module "delete_res_resource" {
   fn_name             = "delete-res"
   http_method         = "POST"
   lambda_role_arn     = aws_iam_role.iam_for_lambda.arn
-  lambda_build        = "32"
+  lambda_build        = "36"
   apigw_id            = aws_api_gateway_rest_api.reservations.id
   root_resource_id    = aws_api_gateway_rest_api.reservations.root_resource_id
   apigw_execution_arn = local.apigw_execution_arn

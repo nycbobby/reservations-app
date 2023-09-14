fc8d40f8bdef65d9ce85c5222a281e7969c5c055
Bobby B <bobby.benninger@gmail.com>
2023-09-13 22:14:06 -0400

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# deploy kubernetes cluster for nginx
 module "eks" {
   source          = "./modules/eks"
   cluster_name    = var.cluster_name
@@ -5,6 +6,7 @@ module "eks" {
   vpc_id          = var.vpc_id
 }
 
+# deploy api gateway and lambdas for reservations app
 module "apigw" {
   source          = "./modules/apigw"
   apigw_name      = var.apigw_name
@@ -12,18 +14,31 @@ module "apigw" {
   apigw_resources = var.apigw_resources
 }
 
+# deploy dynamodb tables for reservations app
 module "customer_tables" {
   source   = "./modules/dynamo_table"
   for_each = toset(var.customers)
   customer = each.key
 }
 
+# configure iam permissions for reservations app
 module "iam" {
   source         = "./modules/iam"
   tfc_secret_arn = var.tfc_secret_arn
   cluster_name   = var.cluster_name
 }
 
+# create ecr repo for nginx docker images
+resource "aws_ecr_repository" "demo" {
+  name                 = "demo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# deploy lambdas to start and shutdown eks cluster on schedule for demo environment
 module "standby_lambdas" {
   source           = "./modules/lambda"
   for_each         = var.standby_lambdas
@@ -36,15 +51,6 @@ module "standby_lambdas" {
   environment_vars = {
     CLUSTER_NAME   = var.cluster_name
     NODEGROUP_NAME = var.node_group_name
-  }
-}
-
-resource "aws_ecr_repository" "demo" {
-  name                 = "demo"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
   }
 }
 
